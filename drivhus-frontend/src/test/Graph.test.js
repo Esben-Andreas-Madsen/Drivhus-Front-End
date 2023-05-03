0815fe3582638039b056f97f9b42fc4f93f4035e
Esben-Andreas-Madsen <316460@via.dk>
2023-05-03 11:39:08 +0200

    .

---
+++ drivhus-frontend/src/test/Graph.test.js
@@ -0,0 +1,7 @@
+import TempGraph from "../components/TempGraph";
+import { cleanup, fireEvent, render } from "@testing-library/react";
+
+it("renders without crashing", () => {
+  const tempgraph = render(<TempGraph />);
+  expect();
+});
