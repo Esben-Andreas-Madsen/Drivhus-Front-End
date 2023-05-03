9f22ca22c9e65f86413c2a288f86315bd869bd0b
Esben-Andreas-Madsen <316460@via.dk>
2023-05-03 09:53:06 +0200

    Routing Complete and ready to be expanded upon

--- drivhus-frontend/src/index.js
+++ drivhus-frontend/src/index.js
@@ -2,11 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import TempGraph from './components/TempGraph';
+import Root from './routes/Root';
+
+
+import { RouterProvider, createHashRouter } from "react-router-dom";
+
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/tempgraph",
+        element: <TempGraph />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <TempGraph/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
