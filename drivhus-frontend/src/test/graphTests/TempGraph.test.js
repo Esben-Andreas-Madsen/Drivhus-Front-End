29d59369788de56b40575c289c1b08acf301cb0c
Esben-Andreas-Madsen <316460@via.dk>
2023-05-04 11:07:06 +0200

    TempGraph Smoke Test

---
+++ drivhus-frontend/src/test/graphTests/TempGraph.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TempGraph from "../../components/TempGraph";
+import "jest-environment-jsdom";
+
+it("renders graph data", () => {
+  const root = ReactDOM.createRoot(document.createElement("div"));
+  root.render(<TempGraph />);
+});
