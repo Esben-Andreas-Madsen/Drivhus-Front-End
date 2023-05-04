e93c6e2a44eab77297e719921297cdc36cc26ff4
Esben-Andreas-Madsen <316460@via.dk>
2023-05-04 11:33:51 +0200

    .

--- drivhus-frontend/src/test/graphTests/TempGraph.test.js
+++ drivhus-frontend/src/test/graphTests/TempGraph.test.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import TempGraph from "../../components/tempGraph/TempGraph";
 import "jest-environment-jsdom";
 
-it("renders graph data", () => {
+it("renders temp graph", () => {
   const root = ReactDOM.createRoot(document.createElement("div"));
   root.render(<TempGraph />);
 });
