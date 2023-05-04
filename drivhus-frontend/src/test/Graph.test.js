d21ccf9d47bd175186d1f1c8d55cead1112b22f3
Esben-Andreas-Madsen <316460@via.dk>
2023-05-04 11:24:56 +0200

    .

--- drivhus-frontend/src/test/Graph.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "../App";
-import "jest-environment-jsdom";
-
-it("renders without crashing", () => {
-  const root = ReactDOM.createRoot(document.createElement("div"));
-  root.render(<App />);
-});
