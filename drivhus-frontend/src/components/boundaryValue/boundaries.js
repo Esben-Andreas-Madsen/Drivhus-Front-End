d9fdc5cf3304dfd04cf8b010d7757fb36412ff7b
benjjjjjj <302301@viauc.dk>
2023-05-11 12:11:50 +0200

    Komponent opdeling

--- drivhus-frontend/src/components/boundaryValue/boundaries.js
+++ drivhus-frontend/src/components/boundaryValue/boundaries.js
@@ -1,39 +1,5 @@
-import styles from "./boundaries.module.css";
+import TempBoundaries from "./tempBoundary/TempBoundaries";
 
 export default function Boundaries() {
-  return (
-    <div className={styles.boundaryBox1}>
-      <h2>Temperatur</h2>
-      <div>
-        <label for="inputField">Maxværdi: </label>
-        <input
-          type="text"
-          id="inputField"
-          name="inputField"
-          placeholder="°C"
-        ></input>
-        <input
-          type="button"
-          id="inputButton"
-          name="inputButton"
-          value={"Opdater"}
-        ></input>
-      </div>
-      <div>
-        <label for="inputField">Minimumværdi: </label>
-        <input
-          type="text"
-          id="inputField"
-          name="inputField"
-          placeholder="°C"
-        ></input>
-        <input
-          type="button"
-          id="inputButton"
-          name="inputButton"
-          value={"Opdater"}
-        ></input>
-      </div>
-    </div>
-  );
+  return <TempBoundaries />;
 }
