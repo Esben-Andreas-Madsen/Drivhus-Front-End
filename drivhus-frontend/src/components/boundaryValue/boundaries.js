b245dbed22e6ce0ca357e5febd77d21487055b5b
benjjjjjj <302301@viauc.dk>
2023-05-11 10:43:33 +0200

    Opdateret grænseværdi branch

--- drivhus-frontend/src/components/boundaryValue/boundaries.js
+++ drivhus-frontend/src/components/boundaryValue/boundaries.js
@@ -1,21 +1,40 @@
-export default function Boundaries(){
-
-
-
-return (
-    <div> 
-    <h2>Temperatur</h2>
+export default function Boundaries() {
+  return (
     <div>
+      <h2>Temperatur</h2>
+      <div>
         <label for="inputField">Maxværdi: </label>
-        <input type="text" id="inputField" name="inputField" placeholder="EX 22"></input>
-        <input type="button" id="inputButton" name="inputButton" value={"Opdater"}></input>
-    </div>
-    <div>
+        <input
+          type="text"
+          id="inputField"
+          name="inputField"
+          placeholder="EX °45"
+        ></input>
+        <input
+          type="button"
+          id="inputButton"
+          name="inputButton"
+          value={"Opdater"}
+        ></input>
+      </div>
+      <div>
         <label for="inputField">Minimumværdi: </label>
-        <input type="text" id="inputField" name="inputField"></input>
-        <button type="button" name="submit" value="Submit" onclick="alert('Temperatur update!')">Opdater</button>
+        <input
+          type="text"
+          id="inputField"
+          name="inputField"
+          placeholder="EX °45"
+        ></input>
+        <button
+          type="button"
+          name="submit"
+          value="Submit"
+          class="my-button-class"
+          onclick="updateTemperature"
+        >
+          Opdater
+        </button>
+      </div>
     </div>
-    </div>
-)
+  );
 }
-
