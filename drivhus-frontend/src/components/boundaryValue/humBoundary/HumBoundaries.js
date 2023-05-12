5e4386cf8d9f2ccd368f4e919edcf82f687a4add
benjjjjjj <302301@viauc.dk>
2023-05-12 10:45:22 +0200

    Added humidity validation

--- drivhus-frontend/src/components/boundaryValue/humBoundary/HumBoundaries.js
+++ drivhus-frontend/src/components/boundaryValue/humBoundary/HumBoundaries.js
@@ -1,5 +1,19 @@
 import styles from "../boundaries.module.css";
 
+function checkInput(input, minOrMax) {
+  if (input % 1 === 0 || input % 1 === 0.5) {
+    if (input !== "" && input < 100 && input > 0) {
+      alert(minOrMax + "humidity updated to: " + input + "%");
+    } else {
+      alert("Please input a number in the range of 0-100");
+    }
+  } else {
+    alert(
+      "Invalid input, please enter a number in the range of 0-100 and in 0.5 - 1 increments"
+    );
+  }
+}
+
 export default function HumBoundaries() {
   return (
     <div className={styles.boundaryBox1}>
@@ -7,31 +21,39 @@ export default function HumBoundaries() {
       <div>
         <label for="inputField">Max Value: </label>
         <input
-          type="text"
-          id="inputField"
-          name="inputField"
+          type="number"
+          id="inputMaxHumField"
           placeholder="%"
+          min="0"
+          max="100"
+          step="0.5"
         ></input>
         <input
           type="button"
-          id="inputButton"
-          name="inputButton"
+          id="inputMaxButton"
           value={"Update"}
+          onClick={() =>
+            checkInput(document.getElementById("inputMaxHumField").value, "max")
+          }
         ></input>
       </div>
       <div>
         <label for="inputField">Min Value: </label>
         <input
-          type="text"
-          id="inputField"
-          name="inputField"
+          type="number"
+          id="inputMinHumField"
           placeholder="%"
+          min="0"
+          max="100"
+          step="0.5"
         ></input>
         <input
           type="button"
-          id="inputButton"
-          name="inputButton"
+          id="inputMinButton"
           value={"Update"}
+          onClick={() =>
+            checkInput(document.getElementById("inputMinHumField").value, "min")
+          }
         ></input>
       </div>
     </div>
