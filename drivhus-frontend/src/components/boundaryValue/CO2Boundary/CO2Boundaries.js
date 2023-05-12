7e7630f0567b99962cd84c49cb0f0b320f5b7b43
benjjjjjj <302301@viauc.dk>
2023-05-12 12:19:58 +0200

    CO2 Value validity & checking

--- drivhus-frontend/src/components/boundaryValue/CO2Boundary/CO2Boundaries.js
+++ drivhus-frontend/src/components/boundaryValue/CO2Boundary/CO2Boundaries.js
@@ -1,15 +1,35 @@
 import styles from "../boundaries.module.css";
 
+// lav et tjek om minimum value er mindre end max value der sættes. ()
+
+// tjekker om input er et tal, om det er i intervallet 0-1000000 og om det incrementes med 5000 + om min > max
 function checkInput(input, minOrMax) {
-  if (input % 1 === 0 && input % 500 === 0 && input >= 0 && input <= 1000000) {
-    if (input !== "" && input < 1000000 && input > 0) {
-      alert(minOrMax + "humidity updated to: " + input + "ppm");
+  const maxInput = document.getElementById("inputMaxCO2Field").value;
+  const minInput = document.getElementById("inputMinCO2Field").value;
+  if (input % 1 === 0 && input >= 400 && input <= 800) {
+    if (input !== "" && input <= 800 && input > 0) {
+      if (minOrMax === "min" && input >= 799) {
+        alert("Minimum value cannot exceed 799.");
+        return;
+      } else if (minOrMax === "max" && input <= 401) {
+        alert("Maximum value cannot be less than 401.");
+        return;
+      } else if (minOrMax === "min" && input >= maxInput) {
+        alert(
+          "Minimum value cannot be greater than or equal to maximum value."
+        );
+        return;
+      } else if (minOrMax === "max" && input <= minInput) {
+        alert("Maximum value cannot be less than or equal to minimum value.");
+        return;
+      }
+      alert(minOrMax + " humidity updated to: " + input + "ppm");
     } else {
-      alert("Please input a ppm value in the range of 0-100000");
+      alert("Please input a ppm value in the range of 400-800");
     }
   } else {
     alert(
-      "Invalid input, please enter a number in the range of 0-100000 and in increments of 1000 "
+      "Invalid input, please enter a number in the range of 400-800 and in increments of 1 "
     );
   }
 }
@@ -24,9 +44,9 @@ export default function CO2Boundaries() {
           type="number"
           id="inputMaxCO2Field"
           placeholder="ppm"
-          min="0"
-          max="1000000"
-          step="500"
+          min="400"
+          max="800"
+          step="1"
         ></input>
         <input
           type="button"
@@ -43,9 +63,9 @@ export default function CO2Boundaries() {
           type="number"
           id="inputMinCO2Field"
           placeholder="ppm"
-          min="0"
-          max="1000000"
-          step="500"
+          min="400"
+          max="800"
+          step="1"
         ></input>
         <input
           type="button"
