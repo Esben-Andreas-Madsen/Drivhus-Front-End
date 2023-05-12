1e937b795c7d98c92acd48011bd8a88846698b60
benjjjjjj <302301@viauc.dk>
2023-05-12 10:21:18 +0200

    Added input validation on min/max button for temp

--- drivhus-frontend/src/components/boundaryValue/tempBoundary/TempBoundaries.js
+++ drivhus-frontend/src/components/boundaryValue/tempBoundary/TempBoundaries.js
@@ -2,9 +2,15 @@ import styles from "../boundaries.module.css";
 
 function checkInput(input, minOrMax) {
   if (input % 1 === 0 || input % 1 === 0.5) {
-    alert(minOrMax + "temperature updated to: " + input + "°C");
+    if (input !== "" && input < 100 && input > 0) {
+      alert(minOrMax + "temperature updated to: " + input + "°C");
+    } else {
+      alert("Please input a number in the range of 0-100");
+    }
   } else {
-    alert("Invalid input, please enter a number incrementing by 0.5 or 1");
+    alert(
+      "Invalid input, please enter a number in the range of 0-100 and in 0.5 - 1 increments"
+    );
   }
 }
 
@@ -14,28 +20,40 @@ export default function TempBoundaries() {
       <h2>Temperature</h2>
       <div>
         <label for="inputField">Max Value: </label>
-        <input type="text" id="inputMaxField" placeholder="°C"></input>
+        <input
+          type="number"
+          id="inputMaxField"
+          placeholder="°C"
+          min="0"
+          max="100"
+          step="0.5"
+        ></input>
         <input
           type="button"
           id="inputMaxButton"
           value={"Update"}
-          onClick={checkInput(
-            document.getElementById("inputMaxField").value,
-            "max"
-          )}
+          onClick={() =>
+            checkInput(document.getElementById("inputMaxField").value, "max")
+          }
         ></input>
       </div>
       <div>
         <label for="inputField">Min Value: </label>
-        <input type="text" id="inputMinField" placeholder="°C"></input>
+        <input
+          type="number"
+          id="inputMinField"
+          placeholder="°C"
+          min="0"
+          max="100"
+          step="0.5"
+        ></input>
         <input
           type="button"
           id="inputMinButton"
           value={"Update"}
-          onClick={checkInput(
-            document.getElementById("inputMinField").value,
-            "min"
-          )}
+          onClick={() =>
+            checkInput(document.getElementById("inputMinField").value, "min")
+          }
         ></input>
       </div>
     </div>
