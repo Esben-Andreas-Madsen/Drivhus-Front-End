24b6725f66c98083a78245e2f36fc6bc3dbd75a8
Esben-Andreas-Madsen <316460@via.dk>
2023-05-17 09:38:49 +0200

    .

---
+++ drivhus-frontend/src/test/boundaryValueTest/tempBoundaryTest.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import TempBoundaries from "../../components/boundaryValue/tempBoundary/TempBoundaries";
+
+const setup = () => {
+  const utils = render(<TempBoundaries />);
+  const maxInput = screen.getByLabelText("inputMaxField");
+  const maxButton = screen.getByLabelText("inputMaxButton");
+  return {
+    maxInput,
+    maxButton,
+    ...utils,
+  };
+};
+
+it("max: accepts 10 as a valid temp", () => {
+  const { maxInput } = setup();
+  fireEvent.change(maxInput, { target: { value: "10" } });
+  expect(maxInput.value).toBe("10");
+});
+
+it("max: rejects 101 as a valid temp", () => {
+  const { maxInput, maxButton } = setup();
+  fireEvent.change(maxInput, { target: { value: "101" } });
+  fireEvent.click(maxButton);
+  expect(
+    screen.getByText("Please input a number in the range of 0-100")
+  ).toBeInTheDocument();
+});
+
+it("max: rejects 50.2 as a valid temp", () => {
+  const { maxInput, maxButton } = setup();
+  fireEvent.change(maxInput, { target: { value: "50.2" } });
+  fireEvent.click(maxButton);
+  expect(
+    screen.getByText(
+      "Invalid input, please enter a number in the range of 0-100 and in 0.5 - 1 increments"
+    )
+  ).toBeInTheDocument();
+});
