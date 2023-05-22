dbd629c0b3a8f8afd01759033c3c71f2d29f9ca8
Esben-Andreas-Madsen <316460@via.dk>
2023-05-22 21:04:22 +0200

    E2E temp validation testing

--- drivhus-frontend/src/test/boundaryValueTest/tempBoundaryTest.test.js
+++ drivhus-frontend/src/test/boundaryValueTest/tempBoundaryTest.test.js
@@ -1,63 +1,106 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, act } from "@testing-library/react";
 import BoundaryValue from "../../components/boundaryValue/BoundaryValue";
+import { unmountComponentAtNode } from "react-dom";
 
-const setup = () => {
-  const utils = render(<BoundaryValue />);
-  const tempMax = screen.getByLabelText("inputMaxTempField");
-  const tempMin = screen.getByLabelText("inputMinTempField");
-  const response = screen.getByLabelText("responseField");
+function dummyValues() {
+  const maxTemp = screen.getByLabelText("inputMaxTempField");
+  fireEvent.change(maxTemp, { target: { value: 100 } });
+
+  const minTemp = screen.getByLabelText("inputMinTempField");
+  fireEvent.change(minTemp, { target: { value: 1 } });
+
+  const maxHumi = screen.getByLabelText("inputMaxHumiField");
+  fireEvent.change(maxHumi, { target: { value: 100 } });
+
+  const minHumi = screen.getByLabelText("inputMinHumiField");
+  fireEvent.change(minHumi, { target: { value: 1 } });
+
+  const maxCO2 = screen.getByLabelText("inputMaxCO2Field");
+  fireEvent.change(maxCO2, { target: { value: 800 } });
+
+  const minCO2 = screen.getByLabelText("inputMinCO2Field");
+  fireEvent.change(minCO2, { target: { value: 400 } });
+}
+
+it("rejects -1", async () => {
+  render(<BoundaryValue />);
+  dummyValues();
+  const maxTemp = screen.getByLabelText("inputMaxTempField");
   const submitBtn = screen.getByLabelText("submitBtn");
-  return {
-    tempMax,
-    tempMin,
-    submitBtn,
-    response,
-    ...utils,
-  };
-};
-
-it("max: rejects 1.1 as a valid temp", () => {
-  const { tempMax, submitBtn, response } = setup();
-  fireEvent.change(tempMax, { target: { value: 1.1 } });
+  const response = screen.getByLabelText("responseField");
+  fireEvent.change(maxTemp, { target: { value: -1 } });
   fireEvent.click(submitBtn);
   expect(response).toHaveTextContent(
-    "Temperature must be in the range of 0-100 and in 0.5 - 1 increments, and max > min."
+    "Temperature must be in the range of 0-100 and in 0.5 - 1 increments, and max > min, max != min."
   );
 });
 
 it("max: rejects 101 as a valid temp", () => {
-  const { tempMax, submitBtn, response } = setup();
-  fireEvent.change(tempMax, { target: { value: 101 } });
+  render(<BoundaryValue />);
+  dummyValues();
+  const maxTemp = screen.getByLabelText("inputMaxTempField");
+  const submitBtn = screen.getByLabelText("submitBtn");
+  const response = screen.getByLabelText("responseField");
+  fireEvent.change(maxTemp, { target: { value: 101 } });
   fireEvent.click(submitBtn);
   expect(response).toHaveTextContent(
-    "Temperature must be in the range of 0-100 and in 0.5 - 1 increments, and max > min."
+    "Temperature must be in the range of 0-100 and in 0.5 - 1 increments, and max > min, max != min."
   );
 });
 
-it("max: rejects -1 as a valid temp", () => {
-  const { tempMax, submitBtn, response } = setup();
-  fireEvent.change(tempMax, { target: { value: -1 } });
+it("max: rejects 100.5 as a valid temp", () => {
+  render(<BoundaryValue />);
+  dummyValues();
+  const maxTemp = screen.getByLabelText("inputMaxTempField");
+  const submitBtn = screen.getByLabelText("submitBtn");
+  const response = screen.getByLabelText("responseField");
+  fireEvent.change(maxTemp, { target: { value: 100.5 } });
   fireEvent.click(submitBtn);
   expect(response).toHaveTextContent(
-    "Temperature must be in the range of 0-100 and in 0.5 - 1 increments, and max > min."
+    "Temperature must be in the range of 0-100 and in 0.5 - 1 increments, and max > min, max != min."
   );
 });
 
 it("max: rejects 0 as a valid temp", () => {
-  const { tempMax, submitBtn, response } = setup();
-  fireEvent.change(tempMax, { target: { value: 0 } });
+  render(<BoundaryValue />);
+  dummyValues();
+  const maxTemp = screen.getByLabelText("inputMaxTempField");
+  const submitBtn = screen.getByLabelText("submitBtn");
+  const response = screen.getByLabelText("responseField");
+  fireEvent.change(maxTemp, { target: { value: 0 } });
   fireEvent.click(submitBtn);
   expect(response).toHaveTextContent(
-    "Temperature must be in the range of 0-100 and in 0.5 - 1 increments, and max > min."
+    "Temperature must be in the range of 0-100 and in 0.5 - 1 increments, and max > min, max != min."
   );
 });
 
-it("max: accepts 100 as a valid temp", () => {
-  const { tempMax, submitBtn, response } = setup();
-  fireEvent.change(tempMax, { target: { value: 100 } });
+it("max: rejects 70.7 as a valid temp", () => {
+  render(<BoundaryValue />);
+  dummyValues();
+  const maxTemp = screen.getByLabelText("inputMaxTempField");
+  const submitBtn = screen.getByLabelText("submitBtn");
+  const response = screen.getByLabelText("responseField");
+  fireEvent.change(maxTemp, { target: { value: 70.7 } });
   fireEvent.click(submitBtn);
   expect(response).toHaveTextContent(
-    "Temperature must be in the range of 0-100 and in 0.5 - 1 increments, and max > min."
+    "Temperature must be in the range of 0-100 and in 0.5 - 1 increments, and max > min, max != min."
+  );
+});
+
+it("max & min: rejects 50 and 50 as a valid temp", () => {
+  render(<BoundaryValue />);
+  dummyValues();
+  const maxTemp = screen.getByLabelText("inputMaxTempField");
+  const minTemp = screen.getByLabelText("inputMinTempField");
+
+  const submitBtn = screen.getByLabelText("submitBtn");
+  const response = screen.getByLabelText("responseField");
+
+  fireEvent.change(maxTemp, { target: { value: 50 } });
+  fireEvent.change(minTemp, { target: { value: 50 } });
+  fireEvent.click(submitBtn);
+  expect(response).toHaveTextContent(
+    "Temperature must be in the range of 0-100 and in 0.5 - 1 increments, and max > min, max != min."
   );
 });
