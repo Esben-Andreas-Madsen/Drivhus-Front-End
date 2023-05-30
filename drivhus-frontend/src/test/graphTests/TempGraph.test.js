72a93dc4ff0e03b2580318faedccc68b69af97c5
Esben-Andreas-Madsen <316460@via.dk>
2023-05-30 14:26:42 +0200

    Updates

--- drivhus-frontend/src/test/graphTests/TempGraph.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import TempGraph from "../../components/graphs/tempGraph/TempGraph";
-import "jest-environment-jsdom";
-
-it("renders temp graph", () => {
-  const root = ReactDOM.createRoot(document.createElement("div"));
-  root.render(<TempGraph />);
-});
